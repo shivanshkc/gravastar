1cbdf621157ac831b372c31dda9a78c81839894e
Shivansh Kuchchal <shivanshbox@gmail.com>
2025-10-22 00:29:12 +0530

    fix(ui): responsive instruction text

--- static/src/index.js
+++ static/src/index.js
@@ -112,6 +112,7 @@ function draw(canvas, engine) {
 
     engine.getDots().forEach(dot => {
         // Scale position.
+        // TODO: Scale radius too.
         const posX = dot.position.x * canvas.width / Resolution;
         const posY = dot.position.y * canvas.height / Resolution;
 
